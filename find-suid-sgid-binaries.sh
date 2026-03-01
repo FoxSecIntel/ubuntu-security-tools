994345a557b65efabc3edfef0799b6823cad2c77
FoxSecIntel <george.crook@gmail.com>
2026-03-01 10:23:51 +0100

    Standardize hidden message blob variable and -m trigger across scripts

--- find-suid-sgid-binaries.sh
+++ find-suid-sgid-binaries.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+__r17q_blob="wqhWaWN0b3J5IGlzIG5vdCB3aW5uaW5nIGZvciBvdXJzZWx2ZXMsIGJ1dCBmb3Igb3RoZXJzLiAtIFRoZSBNYW5kYWxvcmlhbsKoCg=="
+if [[ "${1:-}" == "m" || "${1:-}" == "-m" ]]; then
+  echo "$__r17q_blob" | base64 --decode
+  exit 0
+fi
+
+
 # Script to find SUID/SGID binaries
 
 # Output directory
